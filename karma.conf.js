80d8f36c7cdbfce8ab4836ff7bdf721b3f136dd5
Paul Wilson <paul.i.wilson@ft.com>
2017-03-20 10:28:23 +0000

    guessing at correct config for ios

--- karma.conf.js
+++ karma.conf.js
@@ -2,40 +2,41 @@
 // Generated on Fri Mar 17 2017 08:29:30 GMT+0000 (GMT)
 
 var customLaunchers = {
-	sl_ie_11: {
-		base: 'SauceLabs',
-		browserName: 'internet explorer',
-		platform: 'Windows 8.1',
-		version: '11.0'
-	},
-	sl_ie_9: {
-		base: 'SauceLabs',
-		browserName: 'internet explorer',
-		platform: 'Windows 7',
-		version: '9.0'
-	},
-	sl_edge_13: {
-		base: 'SauceLabs',
-		browserName: 'MicrosoftEdge',
-		platform: 'Windows 10',
-		version: '13.10586'
-	},
+	// sl_ie_11: {
+	// 	base: 'SauceLabs',
+	// 	browserName: 'internet explorer',
+	// 	platform: 'Windows 8.1',
+	// 	version: '11.0'
+	// },
+	// sl_ie_9: {
+	// 	base: 'SauceLabs',
+	// 	browserName: 'internet explorer',
+	// 	platform: 'Windows 7',
+	// 	version: '9.0'
+	// },
+	// sl_edge_13: {
+	// 	base: 'SauceLabs',
+	// 	browserName: 'MicrosoftEdge',
+	// 	platform: 'Windows 10',
+	// 	version: '13.10586'
+	// },
 	sl_ios_9: {
 		base: 'SauceLabs',
 		browserName: 'Safari',
 		appiumVersion: '1.5.3',
 		deviceName: 'iPhone 6 Device',
 		platformVersion: '9.3',
-		platformName: 'iOS'
+		platformName: 'iOS',
+		deviceOrientation: 'portrait'
 	},
-	sl_android_6: {
-		base: 'SauceLabs',
-		browserName: 'Chrome',
-		appiumVersion: '1.5.3',
-		deviceName: 'Samsung Galaxy S6 Device',
-		platformVersion: '6.0',
-		platformName: 'Android'
-	}
+	// sl_android_6: {
+	// 	base: 'SauceLabs',
+	// 	browserName: 'Chrome',
+	// 	appiumVersion: '1.5.3',
+	// 	deviceName: 'Samsung Galaxy S6 Device',
+	// 	platformVersion: '6.0',
+	// 	platformName: 'Android'
+	// }
 }
 
 

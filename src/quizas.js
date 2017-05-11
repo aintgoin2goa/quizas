d27c69c662e7a18bb2f9e56c3217a137e2493c9f
Paul Wilson <paul.i.wilson@ft.com>
2017-05-11 14:38:54 +0100

    adding filter function

--- src/quizas.js
+++ src/quizas.js
@@ -204,6 +204,15 @@ function quizas(obj, path) {
 
 				return extracted;
 			}
+		},
+		'filter': {
+			value: function(func){
+				if(!result.found || !Array.isArray(result.value)){
+					return [];
+				}
+
+				return result.value.filter(func);
+			}
 		}
 	});
 }

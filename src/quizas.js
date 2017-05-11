ebe18c6b09576169314618e16c4a23be3288f4f0
Paul Wilson <paul.i.wilson@ft.com>
2017-05-11 15:44:26 +0100

    adding ability to pass a default value

--- src/quizas.js
+++ src/quizas.js
@@ -102,10 +102,15 @@ function deepWrite(obj, path, value){
 	return root;
 }
 
-function quizas(obj, path) {
+function quizas(obj, path, defaultValue) {
 	var result = typeof path === 'undefined' ?
 		{found: true, value:obj} :
 		deepRead(obj, path);
+
+	if(!result.found && typeof defaultValue !== 'undefined'){
+		result.found = true;
+		result.value = defaultValue;
+	}
 
 	return Object.defineProperties({}, {
 		'value' : {
